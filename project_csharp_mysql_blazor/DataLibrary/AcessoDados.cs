ba3fa49d48b99c94fbc19bb1b61fb479d9f796dc
all-an <all.ancapistan@gmail.com>
2021-11-22 09:53:44 -0300

    entendendo query e conectando db

--- project_csharp_mysql_blazor/DataLibrary/AcessoDados.cs
+++ project_csharp_mysql_blazor/DataLibrary/AcessoDados.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -9,9 +11,24 @@ namespace DataLibrary
 {
     public class AcessoDados
     {
-        public static List<T> LoadData<T, U>(string sql, U parametros, string stringsDeConexao)
+        public List<T> CarregaDado<T, U>(string sql, U parametros, string stringDeConexaoDB)
         {
+            //conexão com o banco de dados
+            using (IDbConnection conexao = new MySqlConnection(stringDeConexaoDB))
+            {
+                List<T> rows = conexao.Query<T>(sql, parametros).ToList();
 
+                return rows;
+            }
+        }
+
+        public void SalvaDado<T>(string sql, T parametros, string stringDeConexaoDB)
+        {
+            //conexão com o banco de dados
+            using (IDbConnection conexao = new MySqlConnection(stringDeConexaoDB))
+            {
+                conexao.Execute(sql, parametros);
+            }
         }
     }
 }
