b942194dcd937084dfe4045f519cc57d08ef1edf
all-an <all.ancapistan@gmail.com>
2021-11-25 12:25:42 -0300

    fim

---
+++ MySQL_scripts/SQLscript.sql
@@ -0,0 +1,14 @@
+INSERT INTO mysqldb.departamento(DepartamentoNome) VALUES ('Desenvolvimento C#');
+UPDATE mysqldb.departamento SET Responsavel = 'Forrest Gump' WHERE DepartamentoId = 1;
+
+
+
+
+
+
+
+
+
+
+
+
